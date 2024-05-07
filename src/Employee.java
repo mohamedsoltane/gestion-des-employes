bf6f5bd712b7d8fb2320085f8dc940a28ff79b02
SoltaneMohamed <soltan.mohamed@esprit.tn>
2024-05-07 12:28:11 +0100

    Prosit 9

---
+++ src/Employee.java
@@ -0,0 +1,78 @@
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String departmentName;
+    private int grade;
+    public Employee() {
+    }
+
+    public Employee(int id, String firstName, String lastName, String departmentName, int grade) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.departmentName = departmentName;
+        this.grade = grade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        Employee employee = (Employee) obj;
+        return id == employee.id && firstName.equals(employee.firstName);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", departmentName='" + departmentName + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}
